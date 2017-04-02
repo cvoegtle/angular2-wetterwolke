1350bf502eb37b837617c7a4a1a45a6f7d2d9695
christian@voegtle.org <christian@voegtle.org>
2017-04-02 17:17:57 +0200

    Testcode ausgebaut

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Location } from "../services/location";
 import { ConfigurationService } from "../services/configuration.service";
 import { Configuration } from "../services/configuration";
 import { CookieService } from "../services/cookie.service";
-import { MdSliderChange } from "@angular/material";
 
 @Component({
   selector: 'app-settings',
@@ -13,8 +12,7 @@ import { MdSliderChange } from "@angular/material";
 export class SettingsComponent implements OnInit {
   public locations: Location[];
   public codeword: string;
-  public frequency: number;
-  private cookieService:CookieService = new CookieService();
+  private cookieService: CookieService = new CookieService();
 
   constructor(private configurationService: ConfigurationService) {
   }
@@ -22,30 +20,29 @@ export class SettingsComponent implements OnInit {
   ngOnInit() {
     this.configurationService.fetchConfiguration().subscribe(configuration => this.extractLocations(configuration));
     this.codeword = this.cookieService.getCodeword();
-    this.frequency = 3;
   }
 
-  private extractLocations(configuration:Configuration):void {
-    let activeLocations:string = this.cookieService.getActiveLocations();
-    let fetchedLocations:Location[] = configuration.locations;
+  private extractLocations(configuration: Configuration): void {
+    let activeLocations: string = this.cookieService.getActiveLocations();
+    let fetchedLocations: Location[] = configuration.locations;
     for (let location of fetchedLocations) {
       location.enabled = activeLocations.indexOf(location.location) >= 0;
     }
-    this.locations=fetchedLocations;
+    this.locations = fetchedLocations;
   }
 
   onCodewordChanged(event) {
     this.cookieService.setCodeword(this.codeword);
   }
 
-  onLocationChanged(locationId: string):void {
+  onLocationChanged(locationId: string): void {
     let activeLocations: string = "";
     for (let location of this.locations) {
       if (location.location == locationId) {
         location.enabled = !location.enabled;
       }
       if (location.enabled) {
-        activeLocations += location.location+",";
+        activeLocations += location.location + ",";
       }
     }
     this.cookieService.setActiveLocations(activeLocations);
