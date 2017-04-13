6854bcbcdfbf8f3d75d13752551ea68acff8f5dd
christian@voegtle.org <christian@voegtle.org>
2017-04-13 17:30:02 +0200

    Apple Launch-Screen rauswerfen

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ng build --prod
-appcache-manifest dist/*.{json,ico,html,js,webapp,css} dist/images/*.png --network-star -p /~christian/ww/app/ -o dist/ww.appcache
+appcache-manifest dist/*.{ico,html,js,webapp,css} dist/images/*.png --network-star -p /~christian/ww/app/ -o dist/ww.appcache
