ee1167f0b275955dd9b5974400198767387e7de2
christian@voegtle.org <christian@voegtle.org>
2019-05-04 17:36:17 +0200

    UV Index, Sonneneinstrahlung und Luftdruck ohne Nachkommastellen anzeigen

--- src/app/number-format.pipe.ts
+++ src/app/number-format.pipe.ts
@@ -6,9 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class NumberFormatPipe implements PipeTransform {
 
   transform(value: number, args?: any): string {
+    let digits = args != undefined ? args  : 1;
     let formatted = "";
     if (value != null) {
-      formatted = value.toFixed(1);
+      formatted = value.toFixed(digits);
     }
     return formatted.replace("\.", ",");
   }
