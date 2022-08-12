5d5f57c207f50fee43dd07c177b9426f1869bfb0
christian@voegtle.org <christian@voegtle.org>
2022-08-12 21:39:18 +0200

    Material Icons selbst hosten

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ng build
-appcache-manifest dist/*.{ico,html,js,webapp,css} dist/images/*.png --network-star -p /~christian/ww/app/ -o dist/ww.appcache
+appcache-manifest dist/*.{ico,html,js,webapp,css} dist/images/*.png dist/assets/*.woff2 --network-star -p /~christian/ww/app/ -o dist/ww.appcache
